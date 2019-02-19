1a7174858684f4e04d2363c905bbd7ed995f8dd1
Anthony José <anthonyasusxbox2014@gmail.com>
2019-02-19 09:06:13 -0300

    text-center animate

--- script/main.js
+++ script/main.js
@@ -1,22 +1,37 @@
 
 var card = document.getElementsByClassName('opc')[0];
 var text = document.getElementsByClassName('text-card')[0];
-var divCardsProjetos = document.querySelectorAll('crd-projetos');
+var divCardsProjetos = document.getElementById('crd-projetos');
+var liBottom = document.getElementById('li-bottom');
+var textCenter = document.getElementById('text-center');
+
+var cards = document.getElementsByClassName('opc');
+
+function textCenterFade() {
+    textCenter.style.transform = "translateY(15em)";
+
+    setTimeout(function () {
+        textCenter.style.transform = "translateY(0)";
+    }, 1000)
+}
+
 
 card.addEventListener("click", function () {
     console.log('click');
     text.style.transform = 'translateY(-3em)';
-
-
+    text.style.transform += 'scale(0.85, 0.85)';
+    text.classList.toggle("text-shadow");
+    divCardsProjetos.classList.toggle("crd-projetos-f");
 }, false);
 
+card.addEventListener("mouseout", function () {
+    text.style.transform = 'translateY(0)';
+    divCardsProjetos.classList.remove("crd-projetos-f");
+}, false);
 
 function show(elementToFade) {
-
     var element = document.querySelector(elementToFade);
     element.classList.add("wrapper-elements-fade-in");
-
-
 }
 
 window.loaded(show('wrapper-elements'));
